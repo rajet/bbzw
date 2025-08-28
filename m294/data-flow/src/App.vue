366376d96fcb1a8205b52757d86927bcdc3f6b37
Rajethan Ranjan <rajethan.ranjan@outlook.com>
2025-08-28 10:47:24 +0200

    Change move exercise to typescript

---
+++ m294/data-flow/src/App.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { faker } from "@faker-js/faker";
+import { ref } from "vue";
+import Post from "./components/Post.vue";
+
+const posts = ref([
+  {
+    id: 1,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: 8,
+  },
+  {
+    id: 2,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: 0,
+  },
+  {
+    id: 3,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: -4,
+  },
+  {
+    id: 4,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: 0,
+  },
+  {
+    id: 5,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: -8,
+  },
+  {
+    id: 6,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: 4,
+  },
+  {
+    id: 7,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: 6,
+  },
+  {
+    id: 8,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: 3,
+  },
+  {
+    id: 9,
+    title: faker.lorem.words(10),
+    text: faker.lorem.paragraph(),
+    user: faker.internet.userName(),
+    votes: 15,
+  },
+]);
+</script>
+
+<template>
+  <div class="container">
+    <Post
+      title="Der Titel dieses Posts"
+      text="Der Text dieses Posts"
+      user="redd-it-user-123"
+      votes="99"
+    />
+  </div>
+</template>
+
+<style scoped>
+body {
+  font-family:
+    -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu,
+    Cantarell, "Helvetica Neue", sans-serif;
+  font-size: 16px;
+  background: #f0f0f0;
+}
+
+.container {
+  max-width: 800px;
+  margin: 40px auto;
+  border: 1px solid #fafafa;
+  box-shadow: 0 0 2px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  border-radius: 4px;
+  position: relative;
+}
+</style>
---
+++ m294/data-flow/src/components/Post.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="post">
+    <div class="post__votes voter">
+      <button class="voter__action">👍</button>
+      <div class="voter__votes">12</div>
+      <button class="voter__action">👎</button>
+    </div>
+    <div class="post__content">
+      <div class="post__user">von Benutzername</div>
+      <div class="post__title">Post Titel</div>
+      <div class="post__text">Post Text</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.post {
+  display: flex;
+  padding: 20px;
+  border: 2px solid transparent;
+}
+
+.post--trending {
+  border: 2px dashed #ecc500;
+  background: #fff8ee;
+}
+
+.post--spam {
+  opacity: 0.25;
+}
+
+.post + .post {
+  border-top: 1px solid #fafafa;
+}
+
+.post__user {
+  font-size: 0.9rem;
+  color: #666;
+  margin-bottom: 10px;
+}
+
+.post__title {
+  font-size: 1rem;
+  line-height: 1.1;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.post__text {
+  color: #666;
+  line-height: 1.2;
+  font-size: 0.9rem;
+}
+
+.post__votes {
+  width: 40px;
+  margin-right: 20px;
+  display: flex;
+  flex-shrink: 0;
+  flex-direction: column;
+  align-items: center;
+}
+
+.voter__votes {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin: 8px 0;
+}
+
+.voter__action {
+  appearance: none;
+  background: none;
+  font-weight: bold;
+  border: none;
+  cursor: pointer;
+  font-size: 1.3rem;
+  transition: background 0.2s ease;
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+  line-height: 1;
+}
+
+.voter__action:hover {
+  background: #f0f0f0;
+}
+</style>
